5ded623a72c7a9318a08da9e3b6f6e46d48ead93
SORAYA YASIN FARAG <soraya_farag@estudante.sesisenai.org.br>
2023-08-31 21:58:56 -0300

    Implementação de métodos de busca no CategoriaRepository e criação da entidade Restaurante e seu Repository

--- src/main/java/br/com/senai/cardapiosmktplaceapi/InitApp.java
+++ src/main/java/br/com/senai/cardapiosmktplaceapi/InitApp.java
@@ -1,37 +1,41 @@
 package br.com.senai.cardapiosmktplaceapi;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.PageRequest;
 
 import br.com.senai.cardapiosmktplaceapi.entity.Categoria;
+import br.com.senai.cardapiosmktplaceapi.entity.Restaurante;
 import br.com.senai.cardapiosmktplaceapi.repository.CategoriasRepository;
+import br.com.senai.cardapiosmktplaceapi.repository.RestaurantesRepository;
 
 @SpringBootApplication
 public class InitApp {
+
+	@Autowired
+	private CategoriasRepository categoriasRepository;
 	
 	@Autowired
-	private CategoriasRepository repository;
+	private RestaurantesRepository restaurantesRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(InitApp.class, args);
 	}
-	
+
 	@Bean
 	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
 		return args -> {
-			/*Categoria novaCategoria = new Categoria();
-			novaCategoria.setNome("Temakeria");
-			novaCategoria.setTipo(TipoDeCategoria.RESTAURANTE);
-			this.repository.save(novaCategoria);*/
-			Categoria categoria = repository.buscarPor(6);
-			System.out.println(categoria.getNome());
-			categoria.setNome("Novo nome");
-			this.repository.save(categoria);
-			System.out.println("Zé FINI");
+			Categoria categoria = categoriasRepository.buscarPor(57);
+			List<Restaurante> restaurantes = restaurantesRepository.listarPor("%rest%", categoria, PageRequest.of(0, 15)).getContent();
+			for (Restaurante r : restaurantes) {
+				System.out.println(r.getNome() + " - " + r.getCategoria().getNome());
+			}
 		};
 	}
 }
