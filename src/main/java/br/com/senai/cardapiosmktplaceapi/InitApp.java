e484ce0f5bbaf1c642d42fd9e33737239a632283
SORAYA YASIN FARAG <soraya_farag@estudante.sesisenai.org.br>
2023-09-14 22:08:14 -0300

    Inicio da camada de controller do projeto

--- src/main/java/br/com/senai/cardapiosmktplaceapi/InitApp.java
+++ src/main/java/br/com/senai/cardapiosmktplaceapi/InitApp.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
+import com.fasterxml.jackson.datatype.hibernate5.jakarta.Hibernate5JakartaModule;
+
 import br.com.senai.cardapiosmktplaceapi.repository.CardapiosRepository;
 import br.com.senai.cardapiosmktplaceapi.repository.CategoriasRepository;
 import br.com.senai.cardapiosmktplaceapi.repository.RestaurantesRepository;
@@ -27,6 +29,11 @@ public class InitApp {
 		SpringApplication.run(InitApp.class, args);
 	}
 
+	@Bean
+	public Hibernate5JakartaModule jsonHibernate5JakartaModule() {
+		return new Hibernate5JakartaModule();
+	}
+	
 	@Bean
 	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
 		return args -> {
