ba79a5e333a03ca514a748191b45f18fb55d139a
SORAYA YASIN FARAG <soraya_farag@estudante.sesisenai.org.br>
2023-10-05 20:58:03 -0300

    Finalização da integração com o serviço de envio de email

--- src/main/java/br/com/senai/cardapiosmktplaceapi/integration/route/ToEmail.java
+++ src/main/java/br/com/senai/cardapiosmktplaceapi/integration/route/ToEmail.java
@@ -4,10 +4,12 @@ import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.http.HttpMethods;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import br.com.senai.cardapiosmktplaceapi.dto.Notificacao;
 import br.com.senai.cardapiosmktplaceapi.integration.processor.ErrorProcessor;
 
 @Component
@@ -28,9 +30,15 @@ public class ToEmail extends RouteBuilder {
 				.process(new Processor() {	
 					@Override
 					public void process(Exchange exchange) throws Exception {
-						
+						Notificacao notificacao = exchange.getMessage().getBody(Notificacao.class);
+						JSONObject notificacaoJson = new JSONObject();
+						notificacaoJson.put("destinatario", notificacao.getDestinatario());
+						notificacaoJson.put("titulo", notificacao.getTitulo());
+						notificacaoJson.put("mensagem", notificacao.getMensagem());
+						exchange.getMessage().setBody(notificacaoJson.toString());
 					}
 				})
+				.toD(urlDeEnvio)              
 			.doCatch(Exception.class)
 				.setProperty("error", simple("${exception}"))
 				.process(errorProcessor)
